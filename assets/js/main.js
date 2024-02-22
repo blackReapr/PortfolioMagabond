6f4da3a898cce5963d8e969f267028585b26e9a7
Hamza Nuruzada <146760833+nrzdhmz@users.noreply.github.com>
2024-02-22 22:38:13 +0400

    Add files via upload
    My Portfoilo Page / not finished

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+  const burger = document.querySelector('.burger');
+  const nav = document.querySelector('nav');
+
+  burger.addEventListener('click', function() {
+    nav.classList.toggle('open');
+  });
+});
